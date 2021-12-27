1be79e713279641d7fb7c0638a43b3fbb8b05a4e
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-12-27 19:03:32 +0900

    초기 커밋.

---
+++ App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UnHandledException
+{
+    /// <summary>
+    /// App.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class App : Application
+    {
+        public static readonly ILog Logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// 정의되지 않은 예외 처리
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Log.Fatal(e.ExceptionObject.ToString());
+
+            MessageBoxResult Result = MessageBox.Show(e.ExceptionObject.ToString());
+            if (Result == MessageBoxResult.OK)
+                this.MainWindow.Close();
+        }
+
+        /// <summary>
+        /// 정의되지 않은 예외 처리(Dispatcher사용 부분)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Log.Fatal(e.Exception.ToString());
+
+            MessageBox.Show(e.Exception.ToString());
+
+        }
+
+    }
+}
